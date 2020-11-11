cd94250d8b2a47feecc9ecfa28135353293a24f1
Maaz Fitter <maazfitter@gmail.com>
2020-11-11 16:22:15 -0600

    Added example screens

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/HomeScreen';
+import SecondScreen from './screens/SecondScreen';
+
+const Navigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      initialRouteName: "Home",
+      defaultNavigationOptions: {
+        title: "App"
+      }
+    },
+    Second: {
+      screen: SecondScreen
+    }
+  }
+);
+
+export default createAppContainer(Navigator);
