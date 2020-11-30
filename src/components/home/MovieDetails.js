752381041c6cf0ccb937b0e627f532c50d8ee258
Maaz Fitter <maazfitter@gmail.com>
2020-11-29 19:36:55 -0600

    Using axios to get movies

--- src/components/home/MovieDetails.js
+++ src/components/home/MovieDetails.js
@@ -1,63 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import vtheatre from "../../api/vtheatre";
 import MovieCard from "./MovieCard";
 
 const MovieDetails = ({ currentDate }) => {
-  const movies = [
-    {
-      id: 1,
-      title: "Black Widow",
-      rating: "PG-13",
-      length: "2 HRS 10 MINS",
-      genre: "ACTION, ADVENTURE, SCI-FI",
-      showtimes: ["07:00 PM", "08:00 PM"],
-      startDate: new Date(2020, 11, 21),
-      endDate: new Date(2020, 11, 21),
-      img: require("../../assets/img/black-widow.jpg"),
-    },
-    {
-      id: 2,
-      title: "Guardians of the Galaxy",
-      rating: "PG-13",
-      length: "2 HRS 1 MIN",
-      genre: "ACTION, ADVENTURE, COMEDY",
-      showtimes: ["07:00 PM", "09:00 PM"],
-      startDate: new Date(2020, 11, 20),
-      endDate: new Date(2020, 11, 20),
-      img: require("../../assets/img/guardians-of-the-galaxy.jpg"),
-    },
-    {
-      id: 3,
-      title: "The Dark Knight Rises",
-      rating: "PG-13",
-      length: "2 HRS 44 MINS",
-      genre: "ACTION, THRILLER",
-      showtimes: ["07:00 PM", "08:00 PM", "09:00 PM", "10:00 PM"],
-      startDate: new Date(2020, 11, 20),
-      endDate: new Date(2020, 11, 25),
-      img: require("../../assets/img/the-dark-knight-rises.jpg"),
-    },
-    {
-      id: 4,
-      title: "Tenet",
-      rating: "PG-13",
-      length: "2 HRS 10 MINS",
-      genre: "ACTION, THRILLER, SCI-FI",
-      showtimes: ["07:00 PM", "08:00 PM"],
-      startDate: new Date(2020, 11, 20),
-      endDate: new Date(2020, 11, 30),
-      img: require("../../assets/img/tenet.jpg"),
-    },
-  ];
+
+  const [movies, setMovies] = useState();
+  const [loaded, setLoaded] = useState([false])
+
+  useEffect(() => {
+
+    const getMoviesApi = async () => {
+      const response = await vtheatre.get('/getMovies');
+      setMovies(response.data);
+      setLoaded(true);
+      };
+
+      getMoviesApi();
+
+  });
 
   return (
     <View style={{ flex: 1 }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        {movies.map((movie) => {
+        {loaded && movies.map((movie) => {
           if (
-            movie.startDate.getDate() <= currentDate.getDate() &&
-            movie.endDate.getDate() >= currentDate.getDate()
+            movie.startDate <= currentDate.getDate() &&
+            movie.endDate >= currentDate.getDate()
           ) {
             return <MovieCard key={movie.id} movie={movie} />;
           }
