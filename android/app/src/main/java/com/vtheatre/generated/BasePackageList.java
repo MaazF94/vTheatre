3e810c305a4043853492a07730afe434b57c4287
Maaz Fitter <maazfitter@gmail.com>
2021-03-12 16:31:22 -0600

    Removing unused expo-location

--- android/app/src/main/java/com/vtheatre/generated/BasePackageList.java
+++ android/app/src/main/java/com/vtheatre/generated/BasePackageList.java
@@ -16,7 +16,6 @@ public class BasePackageList {
         new expo.modules.inapppurchases.InAppPurchasesPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
-        new expo.modules.location.LocationPackage(),
         new expo.modules.network.NetworkPackage(),
         new expo.modules.payments.stripe.StripePackage(),
         new expo.modules.permissions.PermissionsPackage(),
